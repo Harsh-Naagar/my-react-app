b00dbc6c74efe7778516f6b72eec40c0058323bd
Harsh-Naagar <Harshnagar3018@gmail.com>
2025-09-02 17:19:55 +0530

    error solved

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,36 +1,31 @@
-import { createBrowserRouter } from "react-router-dom"; 
+import { createHashRouter } from "react-router-dom";
 import SignUp from "../pages/signup/SignUp";
 import Login from "../pages/login/Login";
-import Layout from "../pages/layout/Layout";
+import Layout from "../pages/layout/layout";
 import Profile from "../pages/Profile";
 import EditProfile from "../pages/editprofile/EditProfile";
 
-export let myRoutes=createBrowserRouter([
-    {
-        path:"/",
-        element:<Layout/>,
-        children:[
-            {
-                path:"/",
-                element:<SignUp/>
-            },
-            {
-                path:"/login",
-                element:<Login/>
-            },
-            {
-                path:"/profile/:id",
-                element:
-                    <Profile/>
-                
-            },
-            {
-                path:"/edit/:id",
-                element:
-                    <EditProfile/>
-                
-                
-            }
-        ]
-    }
-])
+export let myRoutes = createHashRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/profile/:id",
+        element: <Profile />,
+      },
+      {
+        path: "/edit/:id",
+        element: <EditProfile />,
+      },
+    ],
+  },
+]);
