2e64f3609d314dfcf6ae273dd23b674602e17977
Harsh-Naagar <Harshnagar3018@gmail.com>
2025-10-28 20:00:27 +0530

    heart change

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,99 @@
-import { Login } from "../../customhooks/CustomHooks";
+import React, { useState } from "react";
+import style from "./login.module.css";
+import { Link } from "react-router-dom";
+import { useApi } from "../../customhooks/CustomHooks";
 
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+const Login = () => {
+  let [loginuser, setLoginuser] = useState({
+    email: "",
+    password: "",
+  });
+
+  // returns NavigateFunction, helps to navigate between components programmatically
+  let navigate = useNavigate();
+
+  let allSignupusers = useApi("http://localhost:8080/users");
+  console.log(allSignupusers);
+
+  let handleChange = (e) => {
+    let { name, value } = e.target;
+    setLoginuser({ ...loginuser, [name]: value });
+  };
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(loginuser);
+
+    // finding authenticated user
+    let authUser = allSignupusers.find((ele) => {
+      return (
+        ele.email === loginuser.email && ele.password === loginuser.password
+      );
+    });
+    console.log(authUser);
+
+    if (authUser) {
+      // navigate Profile.jsx
+      navigate("/profile/:id");
+      toast.success("login Successfully!")
+
+      // authuser ID save localStorage
+      localStorage.setItem("userid", authUser.id);
+    } else {
+      // navigate Signup.jsx
+      navigate("/");
+      toast.error("you need to signUp!")
+    }
+  };
+
+  return (
+    <main id={style.logincontainer}>
+      <section>
+        <article>
+          <h1>Hello, Friend</h1>
+          <p>
+            Enter your personal details and start <br /> journey with us
+          </p>
+          <Link to={"/"}>
+            <button>SIGN UP</button>
+          </Link>
+        </article>
+        <article>
+          <h1>Sign in</h1>
+          <div className={style.iconscontainer}>
+            <span>heart</span>
+            <span>❤️</span>
+            <span>❤️</span>
+          </div>
+          <p>or use your account</p>
+          <form onSubmit={handleSubmit}>
+            <br />
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={loginuser.email}
+              onChange={handleChange}
+            />
+            <br />
+            <input
+              type="text"
+              placeholder="Password"
+              name="password"
+              value={loginuser.password}
+              onChange={handleChange}
+            />
+            <br />
+            <p>Forgot your password?</p>
+            <button type="submit">SIGN IN</button>
+          </form>
+        </article>
+      </section>
+    </main>
+  );
+};
 
 export default Login;
